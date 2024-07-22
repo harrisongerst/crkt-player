6bb587bf62a054dc4f053a78099803fe6da5b229
harrisongerst <harrisongerstenlauer@gmail.com>
2024-07-22 16:14:55 -0400

    change boilerplate, add timer to console

--- cmd/root.go
+++ cmd/root.go
@@ -8,11 +8,9 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-				  love by spf13 and friends in Go.
-				  Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "crkt",
+	Short: "crkt music",
+	Long: "crkt music and sound player",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("welcome to crkt the command line sound player, try typing 'crkt play'")
 	},
